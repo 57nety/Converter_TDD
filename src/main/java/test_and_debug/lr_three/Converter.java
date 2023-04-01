5939029c3ec4c0eaac0df59609387bef39593c8b
57net <57nety@gmail.com>
2023-04-01 09:42:05 +0700

    Added class Length in OldRusSystem class. oldRusSystemHaveLengthAsInnerClassTest successes

--- src/main/java/test_and_debug/lr_three/Converter.java
+++ src/main/java/test_and_debug/lr_three/Converter.java
@@ -1,10 +1,14 @@
 package test_and_debug.lr_three;
 
 public class Converter {
-    public static class OldRusSystem{
+    public static class OldRusSystem {
+        public static class Length {
+
+        }
 
     }
-    public static class UsaSystem{
+
+    public static class UsaSystem {
 
     }
 }
