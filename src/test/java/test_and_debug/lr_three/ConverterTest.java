215403db4b17282556f876701f35709303a5a4a7
57net <57nety@gmail.com>
2023-04-01 09:23:32 +0700

    Converter creation test

--- src/test/java/test_and_debug/lr_three/ConverterTest.java
+++ src/test/java/test_and_debug/lr_three/ConverterTest.java
@@ -1,7 +1,12 @@
 package test_and_debug.lr_three;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConverterTest {
-
+    @Test
+    public void converterClassCreationTest() throws ClassNotFoundException{
+        Class.forName("test_and_debug.lr_three.Converter");
+    }
 }
