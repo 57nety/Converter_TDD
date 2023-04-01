ee154e7c17c3bda038b1f0e726757cb9497b1564
57net <57nety@gmail.com>
2023-04-01 11:15:54 +0700

    Implemented all methods in the class Converter.OldRusSystem.Length

--- src/main/java/test_and_debug/lr_three/Converter.java
+++ src/main/java/test_and_debug/lr_three/Converter.java
@@ -9,11 +9,8 @@ public class Converter {
              * @param length - длина в межевых верстах
              * @return результат - длина в метрах
              */
-            public static double getFromMejevayaVerstaToSI(double length) throws IllegalArgumentException {
-                //todo реализовать метод позднее
-                if (length == 1.0) return 2134.0;
-
-                throw new IllegalArgumentException("getFromMejevayaVerstaToSI works only for 1 mejevaya versta");
+            public static double getFromMejevayaVerstaToSI(double length) {
+                return length * 2134.0;
             }
 
             /***
@@ -21,11 +18,8 @@ public class Converter {
              * @param length - длина в метрах
              * @return результат - длина в межевых верстах
              */
-            public static double getFromSIToMejevayaVersta(double length) throws IllegalArgumentException {
-                // todo реализовать метод позднее
-                if (length == 1.0) return 1.0 / 2134.0;
-
-                throw new IllegalArgumentException("getFromSIToMejevayaVersta works only for 1 meter");
+            public static double getFromSIToMejevayaVersta(double length) {
+                return length / 2134.0;
             }
 
             /***
@@ -33,11 +27,8 @@ public class Converter {
              * @param length - длина в маховая сажень
              * @return результат - длина в метрах
              */
-            public static double getFromMahovayaSajenToSI(double length) throws IllegalArgumentException {
-                // todo реализовать метод позднее
-                if (length == 1.0) return 1.76;
-
-                throw new IllegalArgumentException("getFromMahovayaSajenToSI works only for 1 mahovaya sajen");
+            public static double getFromMahovayaSajenToSI(double length) {
+                return length * 1.76;
             }
 
             /***
@@ -45,11 +36,8 @@ public class Converter {
              * @param length - длина в метрах
              * @return результат - длина в маховая сажень
              */
-            public static double getFromSIToMahovayaSajen(double length) throws IllegalArgumentException {
-                //todo реализовать метод позднее
-                if (length == 1.0) return 1.0/1.76;
-
-                throw new IllegalArgumentException("getFromSIToMahovayaSajen works only for 1 meter");
+            public static double getFromSIToMahovayaSajen(double length){
+                return length / 1.76;
             }
 
             /***
@@ -57,11 +45,9 @@ public class Converter {
              * @param length - длина в аршин
              * @return результат - длина в метрах
              */
-            public static double getFromArshinToSI(double length) throws IllegalArgumentException {
+            public static double getFromArshinToSI(double length){
                 // todo реализовать метод позднее
-                if(length == 1.0) return 0.7112;
-
-                throw new IllegalArgumentException("getFromArshinToSI works only for 1 arshin");
+                return length * 0.7112;
             }
 
             /***
@@ -69,11 +55,8 @@ public class Converter {
              * @param length - длина в метрах
              * @return результат - длина в аршин
              */
-            public static double getFromSIToArshin(double length) throws IllegalArgumentException {
-                // todo реализовать метод позднее
-                if(length == 1.0) return 1.0/0.7112;
-
-                throw new IllegalArgumentException("getFromArshinToSI works only for 1 arshin");
+            public static double getFromSIToArshin(double length){
+                return length / 0.7112;
             }
         }
 
